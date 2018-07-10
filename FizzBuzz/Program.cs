df845c1f998bea4811c51a405eeda3cc695e603f
Robin Leach <Robin.Leach@softwire.com>
2018-07-10 16:29:09 +0100

    Initial setup, nothing changed

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,24 +10,24 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 101; i++)
+            for (int i = 1; i < 101; i++) // Run through the numbers 1 to 100
             {
-                string output = "";
+                string output = "";     // Start with an empty string
                 if (i % 3 == 0)
                 {
-                    output += "Fizz";
+                    output += "Fizz";   // If the number is divisible by 3, add "Fizz"
                 }
                 if (i % 5 == 0)
                 {
-                    output += "Buzz";
+                    output += "Buzz";    // If the number is divisible by 5, add "Buzz"
                 }
                 if(output == "")
                 {
-                    Console.WriteLine(i);
+                    Console.WriteLine(i); // If the string is still empty (i.e. not div'ble by 3 or 5) just print the number
                 }
                 else
                 {
-                    Console.WriteLine(output);
+                    Console.WriteLine(output);  //...otherwise print the string itself
                 }
             }
             Console.ReadLine();
