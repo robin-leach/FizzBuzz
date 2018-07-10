ec1443064773b64e6a4157bc32496f605893038b
Robin Leach <Robin.Leach@softwire.com>
2018-07-10 16:10:56 +0100

    Basic FizzBuzz functionality

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,29 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+                    }
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
