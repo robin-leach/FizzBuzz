938cfe3fe2001a0975bff01dd812723d6e50bb62
Robin Leach <Robin.Leach@softwire.com>
2018-07-10 16:50:08 +0100

    Added 17 reversibility, with a sort of tedious outer if loop. Seems clunky, could probably be made more efficient

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -13,26 +13,53 @@ namespace FizzBuzz
             for (int i = 1; i < 301; i++) // Run through the numbers 1 to 100
             {
                 string output = "";     // Start with an empty string
-                if (i % 3 == 0)
+                if (i % 17 != 0)        // If it's not divisible by 17 do the normal way around
                 {
-                    output += "Fizz";   // If the number is divisible by 3, add "Fizz"
+                    if (i % 3 == 0)
+                    {
+                        output += "Fizz";   // If the number is divisible by 3, add "Fizz"
+                    }
+                    if (i % 5 == 0)
+                    {
+                        output += "Buzz";    // If the number is divisible by 5, add "Buzz"
+                    }
+                    if (i % 7 == 0)
+                    {
+                        output += "Bang";     // If the number is divisible by 7, add "Bang"
+                    }
+                    if (i % 11 == 0)
+                    {
+                        output = "Bong";      // If the number is divisible by 11, replace with "Bong"
+                    }
+                    if (i % 13 == 0)
+                    {
+                        output = "Fezz" + output;   // If the number is divisible by 13, print "Fezz" and then the string, even in the 11 case
+                    }
                 }
-                if (i % 5 == 0)
+                else // If it's divisible by 17 do the reverse
                 {
-                    output += "Buzz";    // If the number is divisible by 5, add "Buzz"
+                    if (i % 3 == 0)
+                    {
+                        output = "Fizz" + output;   // If the number is divisible by 3, add "Fizz"
+                    }
+                    if (i % 5 == 0)
+                    {
+                        output = "Buzz" + output;    // If the number is divisible by 5, add "Buzz"
+                    }
+                    if (i % 7 == 0)
+                    {
+                        output = "Bang" + output;     // If the number is divisible by 7, add "Bang"
+                    }
+                    if (i % 11 == 0)
+                    {
+                        output = "Bong";      // If the number is divisible by 11, replace with "Bong"
+                    }
+                    if (i % 13 == 0)
+                    {
+                        output += "Fezz";   // If the number is divisible by 13, print "Fezz" and then the string, even in the 11 case
+                    }
                 }
-                if (i % 7 == 0)
-                {
-                    output += "Bang";     // If the number is divisible by 7, add "Bang"
-                }
-                if (i % 11 == 0)
-                {
-                    output = "Bong";      // If the number is divisible by 11, replace with "Bong"
-                }
-                if (i % 13 == 0)
-                {
-                    output = "Fezz" + output;   // If the number is divisible by 13, print "Fezz" and then the string, even in the 11 case
-                }
+
                 if(output == "")
                 {
                     Console.WriteLine(i); // If the string is still empty just print the number
